8cb95788c8a897f5cf5a57cb518721b34f68a7c3
PrzemyslawDev <przemyslawgotowala@gmail.com>
2024-10-18 20:10:35 +0200

    Added plausible reasons for editing schedule

--- Spring/src/main/java/pl/poszkole/PoSzkole/enums/Reason.java
+++ Spring/src/main/java/pl/poszkole/PoSzkole/enums/Reason.java
@@ -1,4 +1,12 @@
 package pl.poszkole.PoSzkole.enums;
 
 public enum Reason {
+    TEACHER_UNAVAILABLE,
+    STUDENT_REQUEST,
+    ROOM_UNAVAILABLE,
+    ROOM_CHANGE,
+    TECHNICAL_ISSUES,
+    HOLIDAY,
+    CLASS_REASSIGNMENT,
+    OTHER
 }
