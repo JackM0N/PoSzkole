28d7c916f3d09381c370a5baf3c0ae5526ba5326
PrzemyslawDev <przemyslawgotowala@gmail.com>
2025-01-28 19:40:37 +0100

    Added missing tests for new subject method

--- Spring/src/test/java/pl/poszkole/PoSzkole/service/SubjectServiceIntegrationTest.java
+++ Spring/src/test/java/pl/poszkole/PoSzkole/service/SubjectServiceIntegrationTest.java
@@ -5,11 +5,16 @@ import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import pl.poszkole.PoSzkole.dto.SubjectDTO;
 import pl.poszkole.PoSzkole.model.Subject;
+import pl.poszkole.PoSzkole.model.WebsiteUser;
+import pl.poszkole.PoSzkole.repository.RoleRepository;
 import pl.poszkole.PoSzkole.repository.SubjectRepository;
+import pl.poszkole.PoSzkole.repository.WebsiteUserRepository;
 
 import java.util.List;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,6 +28,13 @@ public class SubjectServiceIntegrationTest {
     @Autowired
     private SubjectRepository subjectRepository;
 
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private WebsiteUserRepository websiteUserRepository;
+
+    Subject subject;
     Integer numberOfSubjects;
 
     @BeforeEach
@@ -30,7 +42,7 @@ public class SubjectServiceIntegrationTest {
         // Arrange
         numberOfSubjects =  subjectRepository.findAll().size();
 
-        Subject subject = new Subject();
+        subject = new Subject();
         subject.setSubjectName("Przedmiot");
         subjectRepository.save(subject);
     }
@@ -44,5 +56,35 @@ public class SubjectServiceIntegrationTest {
         assertNotNull(subjects);
         assertEquals(numberOfSubjects + 1, subjects.size());
         assertEquals("Przedmiot", subjects.get(numberOfSubjects).getSubjectName());
+    }
+
+    @Test
+    @WithMockUser(username = "teacher")
+    void testGetCurrentTeacherSubjects_Success() {
+        // Arrange
+        Subject differentSubject = new Subject();
+        differentSubject.setSubjectName("Inny Przedmiot");
+        subjectRepository.save(differentSubject);
+
+        WebsiteUser teacher = new WebsiteUser();
+        teacher.setId(9999L);
+        teacher.setUsername("teacher");
+        teacher.setPassword("teacher123");
+        teacher.setFirstName("Teacher");
+        teacher.setLastName("Test");
+        teacher.setEmail("teacher@teacher.com");
+        teacher.setIsDeleted(false);
+        teacher.setGender("M");
+        teacher.setPhone("987654321");
+        teacher.setRoles(Set.of(roleRepository.findByRoleName("TEACHER").get()));
+        teacher.setSubjects(Set.of(subject, differentSubject));
+        websiteUserRepository.save(teacher);
+
+        //Act
+        List<SubjectDTO> subjects = subjectService.getCurrentTeacherSubjects();
+
+        // Assert
+        assertNotNull(subjects);
+        assertEquals(2, subjects.size());
     }
 }
--- Spring/src/test/java/pl/poszkole/PoSzkole/service/SubjectServiceUnitTest.java
+++ Spring/src/test/java/pl/poszkole/PoSzkole/service/SubjectServiceUnitTest.java
@@ -4,13 +4,17 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.Authentication;
 import pl.poszkole.PoSzkole.dto.SubjectDTO;
 import pl.poszkole.PoSzkole.mapper.SubjectMapper;
 import pl.poszkole.PoSzkole.model.Subject;
+import pl.poszkole.PoSzkole.model.WebsiteUser;
 import pl.poszkole.PoSzkole.repository.SubjectRepository;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -22,6 +26,12 @@ public class SubjectServiceUnitTest {
 
     @Mock
     private SubjectMapper subjectMapper;
+
+    @Mock
+    private Authentication authentication;
+
+    @Mock
+    private WebsiteUserService websiteUserService;
 
     @InjectMocks
     private SubjectService subjectService;
@@ -51,5 +61,40 @@ public class SubjectServiceUnitTest {
         assertEquals("Przedmiot", result.get(0).getSubjectName());
         verify(subjectRepository).findAll();
         verify(subjectMapper).toDto(subject);
+    }
+
+    @Test
+    void testGetCurrentTeacherSubjects_Success() {
+        // Arrange
+        Set<Subject> subjects = new HashSet<>();
+        Subject subject = new Subject();
+        subject.setId(1L);
+        subject.setSubjectName("Przedmiot");
+        subjects.add(subject);
+        Subject differentSubject = new Subject();
+        differentSubject.setId(2L);
+        subject.setSubjectName("Inny Przedmiot");
+        subjects.add(differentSubject);
+
+        SubjectDTO subjectDTO = new SubjectDTO();
+        subjectDTO.setId(1L);
+        subjectDTO.setSubjectName("Przedmiot");
+        SubjectDTO differentSubjectDTO = new SubjectDTO();
+        subjectDTO.setId(2L);
+        subjectDTO.setSubjectName("Inny Przedmiot");
+
+        WebsiteUser mockTeacher = new WebsiteUser();
+        mockTeacher.setId(1L);
+        mockTeacher.setSubjects(subjects);
+
+        when(websiteUserService.getCurrentUser()).thenReturn(mockTeacher);
+        when(subjectMapper.toDto(subject)).thenReturn(subjectDTO);
+        when(subjectMapper.toDto(differentSubject)).thenReturn(differentSubjectDTO);
+
+        // Act
+        List<SubjectDTO> result = subjectService.getCurrentTeacherSubjects();
+
+        assertNotNull(result);
+        assertEquals(2, result.size());
     }
 }
